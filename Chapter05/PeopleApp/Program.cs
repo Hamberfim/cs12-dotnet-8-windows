0d3a16b6122f29e63459795579970f686078ab0e
Hamberfim <adhamlin@gmail.com>
2024-06-03 17:01:27 -0500

    Start same book on windows in Visual studio

---
+++ Chapter05/PeopleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using Packt.Shared;  // use Person class from the Packt.Shared namespace
+
+ConfigureConsole();  // sets current culture to default set in Program.Helpers.cs
+
+// alternatives
+// ConfigureConsole(useComputerCulture  : true);  // use the culture set by the OS
+// ConfigureConsole(culture: "fr-FR");  // French (France)
+
+// creatre an instance of the Person class
+Person bill = new();
+// WriteLine(bill.ToString());
+WriteLine(bill);  // implicit call to ToString()
