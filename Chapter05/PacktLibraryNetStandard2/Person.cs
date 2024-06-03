2712a59592133e6484365a53c65e4a21e30c7c62
Hamberfim <adhamlin@gmail.com>
2024-06-03 17:34:26 -0500

    setting/outputting field values

--- Chapter05/PacktLibraryNetStandard2/Person.cs
+++ Chapter05/PacktLibraryNetStandard2/Person.cs
@@ -1,6 +1,10 @@
 ﻿namespace Packt.Shared;
 
-public class Person
+// implicit inheritance from System.Object
+public class Person 
 {
-
+    #region Fields: data or state for this person
+    public string? Name;  // nullable
+    public DateTimeOffset Born;
+    #endregion
 }
--- Chapter05/PeopleApp/Program.cs
+++ Chapter05/PeopleApp/Program.cs
@@ -10,3 +10,29 @@ ConfigureConsole();  // sets current culture to default set in Program.Helpers.c
 Person bill = new();
 // WriteLine(bill.ToString());
 WriteLine(bill);  // implicit call to ToString()
+
+bill.Name = "Bill Smith";
+bill.Born = new DateTimeOffset(
+    year: 1966, 
+    month: 4, 
+    day: 3, 
+    hour: 7, 
+    minute: 29, 
+    second: 0, 
+    offset: TimeSpan.FromHours(-5));  // eastern US time zone
+WriteLine($"{bill.Name} was born on {bill.Born:D}.");  // :D is the long date format
+
+// use named object init syntax
+Person lisa = new()
+{
+    Name = "Lisa Brown",
+    Born = new(
+        year: 1972,
+        month: 3,
+        day: 13,
+        hour: 7,
+        minute: 3,
+        second: 0,
+        TimeSpan.Zero)  // optional offset from UTC
+};
+WriteLine($"{lisa.Name} was born on {lisa.Born}.");
